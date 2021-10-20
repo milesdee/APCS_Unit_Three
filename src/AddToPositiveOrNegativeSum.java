b91c911278c102117a0d38dfe64aba848447e926
milesdee <miles.drabinski@ssfs.org>
2021-10-20 09:53:24 -0400

    if statements :)

--- src/AddToPositiveOrNegativeSum.java
+++ src/AddToPositiveOrNegativeSum.java
@@ -2,17 +2,24 @@ import java.util.Scanner;
 public class AddToPositiveOrNegativeSum {
     public static void main(String[] args) {
         // declare and instantiate a Scanner variable and object
-
+        Scanner scan = new Scanner(System.in);
         // declare and initialize variables for a positive and negative sum
-
+        int posSum = 0;
+        int negSum = 0;
         for (int i = 1; i <= 10; i++) {
             /* in this block, allow the user to input a number and add the number to the appropriate sum */
-
-
+            System.out.println("Enter a number: ");
+            int input = scan.nextInt();
+            if (input > 0){
+                posSum += input;
+            }
+            else{
+                negSum += input;
+            }
         } // end of for loop
-
         // output the sums. Make sure the format matches the sample
-
+        System.out.println("The sum of the positive numbers is " + posSum);
+        System.out.println("The sum of the negative numbers is " + negSum);
     }
 }
 
--- src/FindLargest.java
+++ src/FindLargest.java
@@ -5,10 +5,21 @@ public class FindLargest {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
-
         // Prompt the user for two integer variables
-
+        System.out.print("Give me the first number: ");
+        int firstNumber = scan.nextInt();
+        System.out.print("Give me the second number: ");
+        int secondNumber = scan.nextInt();
         // Create a series of if statements that will print which number is larger or if they are equal
+        if (firstNumber > secondNumber){
+            System.out.println(firstNumber + " is larger than " + secondNumber);
+        }
+        if (firstNumber < secondNumber){
+            System.out.println(secondNumber + " is larger than " + firstNumber);
+        }
+        if (firstNumber == secondNumber){
+            System.out.println(firstNumber + " and " + secondNumber + " are equal");
+        }
         // Make sure your output matches the samples or the tests will not pass.
     }
 }
