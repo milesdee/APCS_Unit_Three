990d69e5c6facf83414b2475853daf5a0b576f53
milesdee <miles.drabinski@ssfs.org>
2021-10-24 17:50:11 -0400

    pig latin and palindrome!

--- src/Palindrome.java
+++ src/Palindrome.java
@@ -1,6 +1,22 @@
 public class Palindrome {
 
-    // Write your isPalindrome method in the space below. You do not need to write a main method for this program
+    // Write your isPalindrome method in the space below.
+    // You do not need to write a main method for this program
     // Your method should be static.
-
+    public static boolean isPalindrome(int number){
+        boolean palin;
+        String numString = String.valueOf(number);
+        System.out.println(numString.length());
+        if (numString.length() == 5){
+            String numReversed = numString.substring(4) + numString.charAt(3) +
+                    numString.charAt(2) + numString.charAt(1) + numString.charAt(0);
+            System.out.println(numString);
+            System.out.println(numReversed);
+            palin = numReversed.equals(numString);
+        }
+        else{
+            palin = false;
+        }
+        return palin;
+    }
 }
--- src/PigLatin.java
+++ src/PigLatin.java
@@ -1,6 +1,48 @@
-public class PigLatin {
+import java.awt.*;
+import java.sql.Array;
+import java.util.List;
+import java.util.Locale;
 
-    public static String makePigLatin(String word) {
-        return "";
+public class PigLatin {
+    public static int firstVowel(String word){
+        word = word.toLowerCase();
+        int firstVowelLocation = Integer.MAX_VALUE;
+        if (word.contains("a")){
+            if (word.indexOf("a") < firstVowelLocation) firstVowelLocation = word.indexOf("a");
+        }
+        if (word.contains("e")) {
+            if (word.indexOf("e") < firstVowelLocation) firstVowelLocation = word.indexOf("e");
+        }
+        if (word.contains("i")){
+            if (word.indexOf("i") < firstVowelLocation) firstVowelLocation = word.indexOf("i");
+        }
+        if (word.contains("o")){
+            if (word.indexOf("o") < firstVowelLocation) firstVowelLocation = word.indexOf("o");
+        }
+        if (word.contains("u")) {
+            if (word.indexOf("u") < firstVowelLocation) firstVowelLocation = word.indexOf("u");
+        }
+        return firstVowelLocation;
     }
+    public static String makePigLatin(String word) {
+        word = word.toLowerCase();
+        int firstVowelLocation = PigLatin.firstVowel(word);
+        String translation;
+        if (firstVowelLocation == 0){
+            translation = word + "yay";
+        }
+        else if (word.contains("y")) {
+            if (word.indexOf("y") != 0){
+                if (word.indexOf("y") < firstVowelLocation) firstVowelLocation = word.indexOf("y");
+                translation = word.substring(firstVowelLocation) + word.substring(0,firstVowelLocation) + "ay";
+            }
+            else translation = word.substring(firstVowelLocation) + word.charAt(0) + "ay";
+
+        }
+        else{
+            translation = word.substring(firstVowelLocation) + word.substring(0,firstVowelLocation) + "ay";
+        }
+        return translation;
+    }
+
 }
